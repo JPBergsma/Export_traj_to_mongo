dc6548185025106b6c14f10ce3087a1d5fa16ce4
Johan Bergsma <29785380+JPBergsma@users.noreply.github.com>
2023-09-23 19:33:59 +0200

    Alpha version for loading data for 3rd iteration trajectories in optimade specification.

--- setup.py
+++ setup.py
@@ -5,11 +5,11 @@ module_dir = Path(__file__).resolve().parent
 
 setup(
     name="Export_traj_to_mongoDB",
-    version="0.1",
+    version="0.2",
     url="",
     license="MIT",
     author="JPBergsma",
-    author_email="johannes.bergsma@epfl.ch",
+    author_email="kwibus@chemist.com",
     description="A tool to extract data from trajectories and put it into a mongoDB",
     long_description=open(module_dir.joinpath("README.md")).read(),
     long_description_content_type="text/markdown",
@@ -19,7 +19,6 @@ setup(
     classifiers=[
         "Development Status :: 2 - Pre-Alpha",
         "Programming Language :: Python :: 3 :: Only",
-        "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: 3.10",
         "Programming Language :: Python :: 3.11",
@@ -27,13 +26,14 @@ setup(
         "Topic :: Database",
         "Topic :: Database :: Database Engines/Servers",
     ],
-    python_requires=">=3.8",
+    python_requires=">=3.9",
     install_requires=[
         "pymongo>=3.12.1",
-        "h5py>=3.7.0",
-        "mdanalysis>=2.2.0",
+        "MDAnalysis>=2.2.0",
         "numpy>=1.23",
         "pymatgen>=2022.7",
-        "optimade @ git+https://github.com/JPBergsma/optimade-python-tools.git@optimade_python_tools_trajectory_0.1"
+        "fastapi>=0.103.0",  # Todo figure out whether this should not be installed already with the installation of the optimade python tools.
+        "jsonlines>=3.1.0",
+        "optimade@git+https://github.com/JPBergsma/optimade-python-tools.git@JPBergsma/trajectory_endpoint"
     ]
 )
